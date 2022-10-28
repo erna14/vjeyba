0ca9b13bd47730a8e10918709a6e366d2540b9a0
Erna Berbic <ernaeniberbic@gmail.com>
2022-10-28 14:43:15 +0200

    Updated project 1.6

--- process.php
+++ process.php
@@ -3,25 +3,28 @@
     if(isset($_SESSION["logedIn"]) && $_SESSION["logedIn"] === true) {
         header('Location: homePage.php');
     }
+    //require('include/data.php');
 
-    require('include/data.php');
+    $dataBaseConn = mysqli_connect("localhost", "Erna", "password1*", "users_db");
 
-    // ucitati podatke iz data.php
-
-    // provjeriti kombinaciju username i pw iz post parametra da li se podudara
-
-    // ako je validno, redirekt na homepage i pozdravna poruka
-
-    // osigurati da ako nije post requet, tj. nema post parametara u prvom planu, redirekcija odmah na login
-
-    global $registeredUsers;
-    $registeredUsersArrayLength = count($registeredUsers);
+    if (!$dataBaseConn) {
+        echo mysqli_connect_error();
+    }
 
     $username = $_POST["username"];
-    $password = $_POST['password'];
+    $password = $_POST["password"];
 
-    $registeredUsername = 'Erna';
-    $registeredPassword = 'blablabla1*';
+    $findUser = "SELECT * FROM tbl_users WHERE username = '$username' AND passoword = '$password'";
+    //$findUserResult = $dataBaseConn->query($findUser);
+    $findUserResult = $dataBaseConn->query($findUser);
+    //global $registeredUsers;
+    //$registeredUsersArrayLength = count($registeredUsers);
+
+    //$username = $_POST["username"];
+    //$password = $_POST['password'];
+
+    //$registeredUsername = 'Erna';
+    //$registeredPassword = 'blablabla1*';
 
     $number = preg_match('@[0-9]@', $password);
     $specialChars = preg_match('@[^\w]@', $password);
@@ -41,16 +44,26 @@
         return;
     }
 
-    for ( $i = 0; $i < $registeredUsersArrayLength; $i++) {
-        if (($username === $registeredUsers[$i]["username"]) && ($password === $registeredUsers[$i]["password"])) {
+    if ($findUserResult) {
+        if (mysqli_num_rows($findUserResult) > 0) {
             $_SESSION["logedIn"] = true;
-            $_SESSION["username"] = $username;
             header('Location: homePage.php?welcome_message=Welcome '.$username);
             return;
         }
     }
 
     header('Location: login.php?error=nema vas');
+
+    //for ( $i = 0; $i < $registeredUsersArrayLength; $i++) {
+      //  if (($username === $registeredUsers[$i]["username"]) && ($password === $registeredUsers[$i]["password"])) {
+            //$_SESSION["logedIn"] = true;
+            //$_SESSION["username"] = $username;
+            //header('Location: homePage.php?welcome_message=Welcome '.$username);
+            //return;
+        //}
+    //}
+
+    //header('Location: login.php?error=nema vas');
 
 
     /*if ( $registeredUsername === $username && $registeredPassword === $password) {
@@ -60,4 +73,5 @@
         header('Location: login.php?error=nema vas');
     }*/
 
+    mysqli_close($dataBaseConn);
  ?>
